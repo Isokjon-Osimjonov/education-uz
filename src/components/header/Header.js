e7d896b1371c196169d49e71b8c949852620233d
Wicked2117 <mygithub2117@gmail.com>
2022-01-19 10:57:56 +0500

    added new folders

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '.././header/Header.css'
+
+function Header() {
+    return (
+        <div>
+            Header section
+        </div>
+    )
+}
+
+export default Header
