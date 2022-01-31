01442cb34f5cb1a35f2251d36feaaa430c41d2d4
Wicked2117 <mygithub2117@gmail.com>
2022-01-31 08:36:44 +0500

    new files added

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../footer/Footer.css'
+function Footer() {
+  return <div></div>;
+}
+
+export default Footer;
---
+++ src/routes/about-us/Aboutus.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../about-us/Aboutus.css'
+function Aboutus() {
+  return <div></div>;
+}
+
+export default Aboutus;
