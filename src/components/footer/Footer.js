02b430a49a14130a4e3de365c23d92e38b3556aa
Wicked2117 <mygithub2117@gmail.com>
2022-02-05 17:12:33 +0500

    footer changed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "../footer/Footer.css";
+import Telegram from '../../assets/images/tg.png'
+import Instagram from '../../assets/images/instagram.png'
+import Facebook from '../../assets/images/facebook.png'
+import Phone from '../../assets/images/phone.png'
 function Footer() {
-
   return (
     <div className="footer">
-      {/* <div className="footer__abowe">SOCIAL MEDIA LINKS</div>
-      <div className="footer__below">
+      <div className="footer__above"> 
+      <p className="social__media"> Our Addresses In Social Networks</p>
+      <div className="social__media__links">
+       <a href="http://www.google.com">  <img  className="telegram" src={Telegram} alt="" />  Telegram</a>  
+       <a href="http://www.google.com">  <img  className="instagram"src={Instagram} alt="" />  Instagram</a> 
+        <a href="http://www.google.com">  <img  className="facebook"src={Facebook} alt="" /> Facebook</a>  
+       <span> <img  className="facebook"src={Phone} alt="" /> Tell: +998 907508090</span>
 
-        Copyright
-      </div> */}
+      </div>
+      </div>
+      <div className="line"></div>
+      <div className="footer__below">
+      <p className="copyright__text">Copyright © 2022 All right reserved By Edu-Uz | <a href="http://www.google.com">Privacy Policy </a>| <a href="http://www.google.com">Terms of Use</a>  | <span>Uzbekistan</span> </p>
+      
+      </div>
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div className="header">
       <div className="header__main__components">
-        <h1 className="header__logo">LEARN ENGLISH</h1>
+        <Link  to="/" className="header__logo">LEARN ENGLISH</Link>
         <ul className="header__components__links__ul">
           <li>
             <Link to="/"  className="header__link"> Home </Link>
