f783da9ed2c6c83ea5df4aedc98c76c4fcbd365e
Wicked2117 <mygithub2117@gmail.com>
2022-02-05 23:52:19 +0500

    home chnaged

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {Typewriter} from 'react-simple-typewriter'
+// import 'react-simple-typewriter/dist/index.css'
 // import Header from "../../components/header/Header";
 import "../home/Home.css";
 // import BcImage from "../../assets/images/Online-Edu.jpg";
@@ -35,7 +37,30 @@ function Home() {
         </div>
       </div>
 
+<div className="home__welcome">
+  <h1 className="welcome__title">Hi! Welcome To LearnEnglish.</h1>
+  <p className="welcome__description">  Learn English online and improve your skills through our high-quality courses and resources – all designed for adult language learners. Everything you find here has been specially created by the Edu-Uz LearnEnglish, the world's English teaching experts.</p>
+</div>
 
+<div className="auto__typing">
+<h1 className="auto__typing__title"> 
+You Can With Us - <span className="typwriter__title"> <Typewriter 
+
+loop
+cursor
+cursorStyle="|"
+typeSpped={60}
+deleteSpeed={30}
+delaySpeed={1000}
+words={["Learn English Online " , "Get Highest IELTS Bands" , "Get  English Books For Free" , "Get Free English Lessons From Experts"]}
+
+
+/>
+
+</span>
+</h1>
+
+</div>
 
     </div>
   );
