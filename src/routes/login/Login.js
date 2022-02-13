b50358e7b25bae0c6e72465eb94a6238cd7a4246
Wicked2117 <mygithub2117@gmail.com>
2022-02-13 18:53:55 +0500

    login added

--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -1,9 +1,45 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import "./Login.css";
+import {Link} from 'react-router-dom'
+import LoginImg from "../../assets/images/loginbc1.png";
+import Google from "../../assets/images/Google.png";
+// import {BiUserCircle} from 'react-icons/bi'
+// import {RiLockPasswordLine} from 'react-icons/ri'
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <div className="login__container">
+      
+        <div className="login__img">
+         
+          <img src={LoginImg} alt="" className="login__main__img" />
+        </div>
+
+        <div className="login__form__container">
+          <div className="login__forms">
+            <h2 className="welcome__title__login">Welcome back to Edu-Uz</h2>
+            <h1 className="login__title">Sign In</h1>
+            <form className="login__inputs">
+              <input type="text" id="email__number" placeholder="Enter your Email or Phone number" />
+              <input type="text" name="" id="password" placeholder="Enter your Password" />
+              <button id="login__button" type="submit">Login</button>
+            </form>
+            <div className="login__social__media">
+              <div className="line__meida"></div> <span>or</span><div className="line__meida"></div>
+            </div>
+            <div className="login__media__icons">
+              <h2>Sign In with Google</h2>
+            <img src={Google} alt="" className="google__link" />
+            </div>
+            <div className="already__user">
+              <h1 className="sue">You don't have an account yet? </h1>
+              <Link className="register__link" to="/register">Register Now</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/routes/register/Register.js
+++ src/routes/register/Register.js
@@ -1,8 +1,44 @@
 import React from 'react'
 import './Register.css'
+import {Link} from 'react-router-dom'
+import registerImg from "../../assets/images/loginbc.png";
+import Google from "../../assets/images/Google.png";
 function Register() {
   return (
-    <div>Register</div>
+    <div className='register__main__container'>
+<div className="register__actions">
+  <div className="register__img">
+  <img src={registerImg} alt="" className="register__main__img" />
+
+  </div>
+
+
+<div className="register__form__container">
+          <div className="register__forms">
+            <h2 className="welcome__title__register">Create an account for Edu-Uz</h2>
+            <h1 className="register__title">Sign Up</h1>
+            <form className="register__inputs">
+              <input type="text" id="register__email__number" placeholder="Enter your Email or Phone number" />
+              <input type="text" name="" id="register__password" placeholder="Enter your Password" />
+              <button id="register__button" type="submit">Register</button>
+            </form>
+            <div className="register__social__media">
+              <div className="line__meida"></div> <span>or</span><div className="line__meida"></div>
+            </div>
+            <div className="register__media__icons">
+              <h2>Sign Up with Google</h2>
+            <img src={Google} alt="" className="google__link" />
+            </div>
+            <div className="already__user">
+              <h1 className="sue">You don't have an account yet? </h1>
+              <Link className="register__link" to="/login">Sign In</Link>
+            </div>
+          </div>
+        </div>
+
+</div>
+
+    </div>
   )
 }
 
